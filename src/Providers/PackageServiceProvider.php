6539629eae023fc1abf639730c6b94a338a6586b
WAME s.r.o <info@wame.sk>
2024-05-28 11:00:10 +0200

    SET migrations in ServiceProvider

--- src/Providers/PackageServiceProvider.php
+++ src/Providers/PackageServiceProvider.php
@@ -31,6 +31,8 @@ class PackageServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        $this->loadMigrationsFrom(__DIR__ . '/../../database/migrations');
+        
         Language::shouldBeStrict();
     }
 }
